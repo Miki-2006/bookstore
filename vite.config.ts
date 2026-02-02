7d0ed5e4e5b4c10929a6b965b1e504fd3e705623
Mirlan Rysbekov <rys5ek0v.111@gmail.com>
2026-02-02 18:13:34 +0900

    Removed base of GitHub pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/bookstore/",
   server: {
     allowedHosts: [
       'apnic-monitor-readily-distributor.trycloudflare.com'
